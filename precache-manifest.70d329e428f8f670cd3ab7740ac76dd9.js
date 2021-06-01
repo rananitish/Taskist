532ef1b6b00e2e48ebb013845a9a3336e2072f61
Nitish Rana <nitishrana@Nitishs-MacBook-Air.local>
2021-06-02 00:53:44 +0530

    Updates

---
+++ precache-manifest.70d329e428f8f670cd3ab7740ac76dd9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d988f2b9d425e0c75f94dee283a3cb6f",
+    "url": "/taskist/index.html"
+  },
+  {
+    "revision": "e5f2aec77137107e26d1",
+    "url": "/taskist/static/js/2.c475bd67.chunk.js"
+  },
+  {
+    "revision": "1db98d3b69bcdef855a6",
+    "url": "/taskist/static/js/main.d09a4b8e.chunk.js"
+  },
+  {
+    "revision": "d392896e60298d821a1b",
+    "url": "/taskist/static/js/runtime-main.f26ccc6d.js"
+  }
+]);
